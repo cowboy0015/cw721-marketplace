d574d42b3030ea3acc1be9a20df90afe732a6c60
cowboy0015 <web3pirate@outlook.com>
2023-10-29 13:08:21 -0500

    project initialized

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use cosmwasm_std::{
+    DepsMut, Env, MessageInfo, Empty, StdResult, Response, entry_point
+};
+ 
+#[entry_point]
+pub fn instantiate(
+	_deps: DepsMut,
+	_env: Env,
+	_info: MessageInfo,
+	_msg: Empty,
+) -> StdResult<Response> {
+	Ok(Response::new())
+}
